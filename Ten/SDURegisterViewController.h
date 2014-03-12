f2161f47f1de434fdad832ddc181f584356c1597
Stanislas Duthoit <stanislas.duthoit@gmail.com>
2014-03-12 19:57:08 -0400

    File structure reorg

---
+++ Ten/SDURegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  SDURegisterViewController.h
+//  Ten
+//
+//  Created by Stanislas Duthoit on 09/03/2014.
+//  Copyright (c) 2014 Stanislas Duthoit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDURegisterViewController : UIViewController
+
+@end
